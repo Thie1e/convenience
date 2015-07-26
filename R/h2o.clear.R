f87cd2d03155832c1155cfe3dd09c400fd2a9919
Thie1e <c.thiele@gmx-topmail.de>
2015-07-26 22:44:35 +0200

    Added h2o.clear

---
+++ R/h2o.clear.R
@@ -0,0 +1,37 @@
+#' Remove objects from an H2O connection (server)
+#'
+#' This function removes all or a specified type of objects from an H2O connection.
+#' @param servername An H2O connection object
+#' @param clear "all" to remove all objects, "models" to remove all model
+#' objects, "noModel" to remove all non-model objects
+#' @keywords H2O h2o.rm
+#' @export
+#' @examples
+#' h2o.clear(h2oServer, "models")
+
+h2o.clear <- function(servername, clear = "all") {
+    stopifnot(clear %in% c("all", "models", "noModel"))
+    stopifnot(class(servername) == "H2OConnection")
+
+    require(stringr)
+    ls_h2o <- h2o.ls(servername)
+    ls_h2o <- as.character(ls_h2o$key)
+
+    if (length(ls_h2o) < 1) stop("No objects at specified H2O connection")
+
+    if (clear == "all") {
+        h2o.rm(conn = servername, ids = as.character(ls_h2o))
+    }
+
+    if (clear == "models") {
+        modelIndices <- str_detect(ls_h2o, "DeepLearning|GBM|K-means|GLM|DRF|
+                                             NaiveBayes|PCA")
+        h2o.rm(conn = servername, ids = as.character(ls_h2o[modelIndices]))
+    }
+
+    if (clear == "noModels") {
+        modelIndices <- str_detect(ls_h2o, "DeepLearning|GBM|K-means|GLM|DRF|
+                                             NaiveBayes|PCA")
+        h2o.rm(conn = servername, ids = as.character(ls_h2o[!modelIndices]))
+    }
+}
