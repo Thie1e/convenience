3eee3c8a9148691606b126f82b8821460a238750
Thie1e <c.thiele@gmx-topmail.de>
2015-07-26 22:53:28 +0200

    Added clear.R

---
+++ R/clear.R
@@ -0,0 +1,11 @@
+#' Remove all objects from an environment
+#'
+#' This function removes all objects from the global environment (default)
+#' or from a specified environment.
+#' @param env The environment from which all objects are to be removed
+#' @keywords rm
+#' @export
+#' @examples
+#' clear()
+
+clear <- function(env = ".GlobalEnv") rm(list = ls(name = env))
