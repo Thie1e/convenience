2827fc0bd4ba625a3aaa0e1293599d513ffa7c39
Thie1e <c.thiele@gmx-topmail.de>
2015-08-07 01:41:52 +0200

    Added maxMDcut

--- R/maxMDcut.R
+++ R/maxMDcut.R
@@ -1,27 +1,34 @@
-#' Determine the optimal cutoff to maximize the sum of all observations
+#' Determine the optimal cutoff to maximize the Mean divided by Standard Deviation
 #'
 #' Given a vector of obsevations and a vector of probabilities / predictions,
 #' this function determines the optimal cutoff for binary classification
-#' to maximize the sum of the given observations. If multiple cutoffs
-#' lead to the maximum sum, the average of these cutoffs is returned.
+#' to maximize the mean of the given observations divided by the standard
+#' deviation of the observations. If multiple cutoffs
+#' lead to the maximum, the minimum, maximum or average of these cutoffs is returned.
 #'
 #' Returns the determined optimal cutoff, a numeric vector of length 1.
 #' @param obs A numeric vector of observations
 #' @param preds A numeric vector of predictions or probabilities in
 #' the interval [0,1]
 #' @param stepsize The interval at which cutoffs are created and examined
+#' @param summaryFunc If multiple cutoffs lead to the optimal solution,
+#' return the "mean", "min" or "max" of those cutoffs
 #' @keywords cutoff threshold classification
 #' @export
 
-maxSumCut <- function(obs, preds, stepsize = 0.02, summaryFunc = "mean") {
+maxMDcut <- function(obs, preds, stepsize = 0.02, summaryFunc = "mean") {
     if (max(preds) > 1 | min(preds) < 0) {
         stop("Predictions / Probabilities should be in [0,1]")
     }
+    stopifnot(summaryFunc %in% c("max", "min", "mean"))
 
     cuts <- seq(0, 1, stepsize)
-    sums <- sapply(cuts, function(cut) sum(obs * (preds >= cut)))
-    results <- data.frame(Cutoff = cuts, Sum = sums)
-    bestCutoff <- with(results, Cutoff[which(Sum == max(Sum))])
+    MeanOverSD <- sapply(cuts, function(cut) {
+        mean(obs * (preds >= cut)) / sd(obs * (preds >= cut))
+    })
+
+    results <- data.frame(Cutoff = cuts, MeanOverSD = MeanOverSD)
+    bestCutoff <- with(results, Cutoff[which(MeanOverSD == max(MeanOverSD))])
 
     # If multiple cutoffs lead to the optimal result
     if (summaryFunc == "mean") {
